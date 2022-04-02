e67282fb7135fc9e5f22fd1408680e564897310f
Rafael Dias <s.rafaeldias@protonmail.com>
2022-04-02 20:20:35 -0300

    feat(telescope): remove "origin/" when creating a branch from remote

--- lua/telescope/_extensions/git_worktree.lua
+++ lua/telescope/_extensions/git_worktree.lua
@@ -1,14 +1,14 @@
-local Path = require("plenary.path")
-local Window = require("plenary.window.float")
-local strings = require("plenary.strings")
-local pickers = require("telescope.pickers")
-local finders = require("telescope.finders")
-local actions = require("telescope.actions")
-local utils = require("telescope.utils")
-local action_set = require("telescope.actions.set")
-local action_state = require("telescope.actions.state")
+local Path = require "plenary.path"
+local Window = require "plenary.window.float"
+local strings = require "plenary.strings"
+local pickers = require "telescope.pickers"
+local finders = require "telescope.finders"
+local actions = require "telescope.actions"
+local utils = require "telescope.utils"
+local action_set = require "telescope.actions.set"
+local action_state = require "telescope.actions.state"
 local conf = require("telescope.config").values
-local git_worktree = require("git-worktree")
+local git_worktree = require "git-worktree"
 
 local force_next_deletion = false
 
@@ -28,11 +28,11 @@ end
 local toggle_forced_deletion = function()
     -- redraw otherwise the message is not displayed when in insert mode
     if force_next_deletion then
-        print('The next deletion will not be forced')
-        vim.fn.execute('redraw')
+        print "The next deletion will not be forced"
+        vim.fn.execute "redraw"
     else
-        print('The next deletion will be forced')
-        vim.fn.execute('redraw')
+        print "The next deletion will be forced"
+        vim.fn.execute "redraw"
         force_next_deletion = true
     end
 end
@@ -42,15 +42,15 @@ local delete_success_handler = function()
 end
 
 local delete_failure_handler = function()
-    print("Deletion failed, use <C-f> to force the next deletion")
+    print "Deletion failed, use <C-f> to force the next deletion"
 end
 
 local ask_to_confirm_deletion = function(forcing)
     if forcing then
-        return vim.fn.input("Force deletion of worktree? [y/n]: ")
+        return vim.fn.input "Force deletion of worktree? [y/n]: "
     end
 
-    return vim.fn.input("Delete worktree? [y/n]: ")
+    return vim.fn.input "Delete worktree? [y/n]: "
 end
 
 local confirm_deletion = function(forcing)
@@ -64,7 +64,7 @@ local confirm_deletion = function(forcing)
         return true
     end
 
-    print("Didn't delete worktree")
+    print "Didn't delete worktree"
     return false
 end
 
@@ -76,15 +76,14 @@ local delete_worktree = function(prompt_bufnr)
     local worktree_path = get_worktree_path(prompt_bufnr)
     actions.close(prompt_bufnr)
     if worktree_path ~= nil then
-       git_worktree.delete_worktree(worktree_path, force_next_deletion, {
-           on_failure = delete_failure_handler,
-           on_success = delete_success_handler
-       })
+        git_worktree.delete_worktree(worktree_path, force_next_deletion, {
+            on_failure = delete_failure_handler,
+            on_success = delete_success_handler,
+        })
     end
 end
 
 local create_input_prompt = function(cb)
-
     --[[
     local window = Window.centered({
         width = 30,
@@ -105,34 +104,36 @@ local create_input_prompt = function(cb)
     --]]
     --
 
-    local subtree = vim.fn.input("Path to subtree > ")
+    local subtree = vim.fn.input "Path to subtree > "
     cb(subtree)
 end
 
 local create_worktree = function(opts)
     opts = opts or {}
     opts.attach_mappings = function()
-        actions.select_default:replace(
-            function(prompt_bufnr, _)
-                local selected_entry = action_state.get_selected_entry()
-                local current_line = action_state.get_current_line()
+        actions.select_default:replace(function(prompt_bufnr, _)
+            local selected_entry = action_state.get_selected_entry()
+            local current_line = action_state.get_current_line()
 
-                actions.close(prompt_bufnr)
+            actions.close(prompt_bufnr)
 
-                local branch = selected_entry ~= nil and
-                    selected_entry.value or current_line
+            local branch = selected_entry ~= nil and selected_entry.value or current_line
 
-                if branch == nil then
-                    return
+            if branch == nil then
+                return
+            end
+
+            create_input_prompt(function(name)
+                if name == "" then
+                    name = branch
                 end
 
-                create_input_prompt(function(name)
-                    if name == "" then
-                        name = branch
-                    end
-                    git_worktree.create_worktree(name, branch)
-                end)
+                if vim.startswith(branch, "origin/") then
+                    branch = string.sub(branch, string.len "origin/" + 1)
+                end
+                git_worktree.create_worktree(name, branch)
             end)
+        end)
 
         -- do we need to replace other default maps?
 
@@ -143,12 +144,12 @@ end
 
 local telescope_git_worktree = function(opts)
     opts = opts or {}
-    local output = utils.get_os_command_output({"git", "worktree", "list"})
+    local output = utils.get_os_command_output { "git", "worktree", "list" }
     local results = {}
     local widths = {
         path = 0,
         sha = 0,
-        branch = 0
+        branch = 0,
     }
 
     local parse_line = function(line)
@@ -162,7 +163,7 @@ local telescope_git_worktree = function(opts)
         if entry.sha ~= "(bare)" then
             local index = #results + 1
             for key, val in pairs(widths) do
-                if key == 'path' then
+                if key == "path" then
                     local new_path = utils.transform_path(opts, entry[key])
                     local path_len = strings.strdisplaywidth(new_path or "")
                     widths[key] = math.max(val, path_len)
@@ -221,14 +222,13 @@ local telescope_git_worktree = function(opts)
             map("n", "<c-f>", toggle_forced_deletion)
 
             return true
-        end
+        end,
     }):find()
 end
 
-return require("telescope").register_extension(
-           {
-        exports = {
-            git_worktrees = telescope_git_worktree,
-            create_git_worktree = create_worktree
-        }
-    })
+return require("telescope").register_extension {
+    exports = {
+        git_worktrees = telescope_git_worktree,
+        create_git_worktree = create_worktree,
+    },
+}
